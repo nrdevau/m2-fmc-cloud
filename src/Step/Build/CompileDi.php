2fc62a8d5158de5cdd1dfb17706e06c6d77297b9
Nathaniel Rogers <nate@nrdev.au>
2024-08-26 16:45:51 +1000

    Always run di:compile verbosely

--- src/Step/Build/CompileDi.php
+++ src/Step/Build/CompileDi.php
@@ -2,20 +2,65 @@
 
 namespace Nrdev\FmcDeployments\Step\Build;
 
-use Magento\MagentoCloud\Filesystem\FileSystemException;
+use Magento\MagentoCloud\App\Error;
+use Magento\MagentoCloud\Config\ConfigException;
 use Magento\MagentoCloud\Step\StepException;
 use Magento\MagentoCloud\Step\StepInterface;
+use Magento\MagentoCloud\Shell\MagentoShell;
+use Magento\MagentoCloud\Shell\ShellException;
+use Magento\MagentoCloud\Shell\ShellFactory;
+use Psr\Log\LoggerInterface;
+use Magento\MagentoCloud\Config\Stage\BuildInterface;
 
 class CompileDi implements StepInterface
 {
+
+    /**
+     * @var LoggerInterface
+     */
+    private $logger;
+
+    /**
+     * @var MagentoShell
+     */
+    private $magentoShell;
+
+    /**
+     * @var BuildInterface
+     */
+    private $stageConfig;
+
+    /**
+     * @param LoggerInterface $logger
+     * @param ShellFactory $shellFactory
+     * @param BuildInterface $stageConfig
+     */
+    public function __construct(
+        LoggerInterface $logger,
+        ShellFactory $shellFactory,
+        BuildInterface $stageConfig
+    ) {
+        $this->logger = $logger;
+        $this->magentoShell = $shellFactory->createMagento();
+        $this->stageConfig = $stageConfig;
+    }
+
     public function execute()
     {
         try {
             $this->logger->info('Output the di compilation issues');
-            #Do some actions
-            $this->writer->update(['MAGE_MODE' => 'production']);
-        } catch (FileSystemException $exception) {
-            throw new StepException($exception->getMessage(), $exception->getCode(), $exception);
+            $this->logger->notice('Running DI compilation');
+            $this->magentoShell->execute(
+                'setup:di:compile',
+                [
+                    '-vvv'
+                ]
+            );
+            $this->logger->notice('End of running DI compilation');
+        } catch (ConfigException $e) {
+            throw new StepException($e->getMessage(), $e->getCode(), $e);
+        } catch (ShellException $e) {
+            throw new StepException($e->getMessage(), Error::BUILD_DI_COMPILATION_FAILED, $e);
         }
     }
 
